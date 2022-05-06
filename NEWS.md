2b31433ee3eb40cd46d19896d2d96c1311f9e2fc
Toshikazu Matsumura <matutosi@gmail.com>
2022-05-06 06:00:10 +0000

    version 0.9.4

--- NEWS.md
+++ NEWS.md
@@ -1,10 +1,29 @@
 # moranajp release news
 
+
+# moranajp 0.9.4.9000
+
+* Todo
+    * Deprecate `mecab()` and `mecab_all()`.
+    * Add `method` argument in `mecab()` and `mecab_all()` 
+      to be able to use "sudachi"
+* Done
+
+# moranajp 0.9.4
+
+* Can apply over 8000 length strings.
+    * `make_groups()`
+    * `make_groups_sub()`
+    * `max_sum_str_length()`
+* Use `purrr::map()` in `moranajp()`
+
 # moranajp 0.9.3
 
-* improve functions
-* add tests by testthat
-* add data-raw
+* Improve functions.
+    * `moranajp_all()` <- `mecab_all()`
+    * `moranajp()` <- `mecab()`
+* Add tests by testthat
+* Add data-raw
 
 # moranajp 0.9.2
 
@@ -17,9 +36,6 @@
 #  moranajp 0.9.0
 
 * First release
-
 * `mecab()`, `mecab_all()` : main functions for morphological analysis using 'MeCab'. Can use data.frame. 
-
 * `add_text_id()`: internal function. 
-
 * `neko`: The first part of 'I Am a Cat' by Soseki Natsume
---
+++ R/make_group.R
@@ -0,0 +1,42 @@
+#' Make groups by splitting string length
+#'
+#' @inherit moranajp_all
+#' @param length A numeric.
+make_groups <- function(tbl, text_col = "text", length = 8000) {
+  len_total <- sum(stringr::str_length(tbl[[text_col]]))
+  ratio <- seq(from = 0.8, to = 0.3, by = -0.1)
+
+  for(i in seq_along(ratio)){
+    len_split <- round(length * ratio[i], 0)
+    n_group <- ceiling(len_total / len_split)
+    res <- make_groups_sub(tbl, text_col, n_group)
+
+    len_max <- max(res$str_length)
+    if(len_max > length){
+      stop(paste0("Max length of text is over the limit\n", "lim: ", length, "\n", "max: ", len_max))
+    }
+
+    len_sum_max <- max_sum_str_length(res)
+    if(len_sum_max < length) return(dplyr::select(res, ! dplyr::all_of("str_length")))
+  }
+  dplyr::select(res, ! dplyr::all_of("str_length"))
+}
+
+#' @rdname make_groups
+#' @param n_group A numeric.
+make_groups_sub <- function(tbl, text_col, n_group) {
+  nr <- nrow(tbl)
+  res <- 
+    tbl %>%
+    dplyr::mutate(
+      gr = dplyr::ntile(1:nr, n_group), 
+      str_length = stringr::str_length(.data[[text_col]]))
+}
+
+#' @rdname make_groups
+max_sum_str_length <- function(tbl) {
+  tbl %>%
+    dplyr::group_by(.data[["gr"]]) %>%
+    dplyr::summarise(sum = sum(.data[["str_length"]])) %>%
+    dplyr::summarise(max(sum))
+}
